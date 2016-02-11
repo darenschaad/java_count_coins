ceda76b2ef7252667f91d2a08c6d22bd5c145f7f
Daren Schaad and Matt Rosanio <darenschaad@gmail.com>
2016-02-11 08:26:11 -0800

    Setup initial files and folders

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,28 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+  public WebDriver getDefaultDriver() {
+      return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Test
+  public void rootTest() {
+      goTo("http://localhost:4567/");
+      assertThat(pageSource()).contains("");
+  }
+}
